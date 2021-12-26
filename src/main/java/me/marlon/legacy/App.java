356188e0d6f28ef481cea212af95383738713f4f
Marlon Santos <marlongamamacedo@gmail.com>
2021-12-25 21:16:04 -0300

    update

--- src/main/java/me/marlon/legacy/App.java
+++ src/main/java/me/marlon/legacy/App.java
@@ -4,14 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import me.marlon.legacy.data.account.User;
+import me.marlon.legacy.listener.Listeners;
 
 import java.io.File;
 
 public class App extends Application {
+
+    public static final String localHistory = "C:/Users/Marlon/IdeaProjects/legacy/src/main/java/me/marlon",
+    title = "Login System",
+    version = "1.0v";
 
     public static void main(String[] args) {
         launch();
@@ -22,18 +28,19 @@ public class App extends Application {
         long start = System.currentTimeMillis();
         System.out.println("[AVISO] Iniciando o projeto.");
 
-        Parent parent = FXMLLoader.load(new File("C:/Users/Marlon/IdeaProjects/legacy/src/main/java/me/marlon/legacy/style/index.fxml").toURL());
+        Parent parent = FXMLLoader.load(new File(localHistory + "/legacy/style/index.fxml").toURL());
         stage.setScene(new Scene(parent));
         stage.setResizable(false);
         stage.getScene().setFill(Color.TRANSPARENT);
         stage.initStyle(StageStyle.TRANSPARENT);
+        stage.getIcons().add(new Image("file:///" + localHistory + "/legacy/style/image/logo.png"));
+        stage.setTitle(title);
 
         User user = new User("Marlon");
         user.setName();
 
+        Listeners.stage = stage;
         stage.show();
-
-
         System.out.println("[AVISO] Projeto iniciado com sucesso. [" + (System.currentTimeMillis() - start) + " ms]");
-        }
     }
+}
--- src/main/java/me/marlon/legacy/listener/Listeners.java
+++ src/main/java/me/marlon/legacy/listener/Listeners.java
@@ -3,7 +3,11 @@ package me.marlon.legacy.listener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import me.marlon.legacy.App;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,10 +15,32 @@ import java.util.ResourceBundle;
 public class Listeners implements Initializable {
 
     @FXML
+    private AnchorPane sidebar;
+
+    @FXML
+    private HBox sidebar_top;
+
+    @FXML
     private Button minimized, exit;
+
+    @FXML
+    private Label title;
+
+    public static Stage stage;
+    public double x = 0, y = 0;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        title.setText(App.title + " " + App.version);
+        sidebar_top.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        sidebar_top.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+
         minimized.setOnMouseClicked(event -> {
             final Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
             stage.setIconified(true);
@@ -23,6 +49,7 @@ public class Listeners implements Initializable {
             final Stage stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
             stage.close();
         });
+
     }
 
 }
